7cae4bc57cde5be25f78ee6e8787242511b87f56
Lucas_Yi <423188112@qq.com>
2021-01-30 21:53:10 +1100

    enable to restore cart from localStorage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import {render} from "react-dom"
 import {BrowserRouter, Switch, Route} from "react-router-dom"
 import Navbar from "./pages/Navbar.js"
@@ -8,19 +8,31 @@ import Products from "./pages/Products.js"
 import Cart from "./pages/Cart.js"
 import ProductDetails from "./entity/ProductDetails.js";
 import "./index.css"
-// import Button from "./Button.js"
-// import Input from "./Input.js"
 
 function App() {
 
-    const [cart, setCart] = useState([])
+    const [cart, setCart] = useState(function () {
+        let savedCart = [];
+        try {
+          savedCart = JSON.parse(localStorage.getItem('cart')) || [];
+        } catch (error) {
+          savedCart = [];
+        }
+        return savedCart;
+      });
+
+    useEffect(() => {
+       if (cart) {
+          localStorage.setItem("cart", JSON.stringify(cart));
+        }
+    }, [cart]);
 
     function handleProductDelete(id) {
         const updatedCart = cart.filter(product=>product.id!==id)
         setCart(updatedCart)
       }
     
-      function handleProductAdd(newProduct) {
+    function handleProductAdd(newProduct) {
         const newProductExist = cart.find(product=>product.id === newProduct.id)
         if(newProductExist){
           const updatedCart = cart.map(product=>{
@@ -66,33 +78,6 @@ function App() {
             </BrowserRouter>
         </div>
     )
-
-
-    // function handleButtonClick() {
-    //     console.log("Button clicked");
-    // }
-
-    // function handleLastNameInput() {
-    //     console.log("Last name changed");
-    // }
-
-    // return (
-    //     <Container>
-    //         <Link href="https://react-tutorial.app">Shop online</Link>
-    //         {/*test button*/}
-    //         <div style={{display: "flex", alignItems: "center", gap: "20px", flexWrap: "wrap"}}>
-    //             <Button>Normal</Button>
-    //             <Button outline>Outline</Button>
-    //             <Button className="extra-class" onClick={handleButtonClick}>Customizable</Button>
-    //         </div>
-    //         {/*test input*/}
-    //         <div style={{display: "flex", alignItems: "center", gap: "20px", flexWrap: "wrap"}}>
-    //             <Input placeholder="First name" />
-    //             <Input placeholder="Last name" onInput={handleLastNameInput} />
-    //             <Input placeholder="Email" type="email" required />
-    //         </div>
-    //     </Container>
-    // )
 }
 
 render(<App />, document.getElementById("react-root"));
