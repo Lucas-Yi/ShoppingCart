3cf90925c3fc510f9ce225c09dd2fd3b720e415b
Lucas_Yi <423188112@qq.com>
2021-01-30 22:42:59 +1100

    embedded with Stripe Checkout
    something wrong with the price_id from API thus we got Error {message: "No such plan: 'price_1HuavSGuhXEITAut56IgndJf'"} for now

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
+import {loadStripe} from "@stripe/stripe-js"
+import Button from "../component/Button.js";
+import Input from "../component/Input.js"
 
 export default function Cart(props) {
+  const [email, setEmail] = useState("")
+
+  const stripeLoadedPromise = loadStripe('pk_test_51IEDKRFd6pPKD4YePVjkBKPexOdlFh6FJTYnnoUPLZdgotEFYhzta75jLNnvdiks3SLpPO8oOKfLs7IqF00K3qHR00rNpThsZB');
+
   const { cart } = props
   const totalQuantity = cart.reduce(
     (total, product) => total + product.quantity,
@@ -10,47 +17,89 @@ export default function Cart(props) {
     (total, product) => total + product.price * product.quantity, 
     0
   )
+
+  const lineItems = cart.map(product=>{
+      return {
+        price: product.price_id, 
+        quantity: product.quantity
+      }
+    }
+  )
+
+  function handleEmailChange(e){
+    setEmail(e.target.value)
+  }
+
+  async function handlePayClick(event){
+    event.preventDefault();
+    const stripe = await stripeLoadedPromise
+    try{
+        console.log(lineItems)
+        const result = await stripe.redirectToCheckout({
+          lineItems:lineItems,
+          mode:"payment",
+          successUrl:"http://localhost:3000/cart",
+          cancelUrl:"http://localhost:3000/cart",
+          customerEmail: email
+        })
+        // this will only log if the redirect did not work
+        console.log(result.error)
+    }catch(error){ 
+        // will see error if wrong API key is put
+        console.log(error)
+    }
+  }
   return (
     <div className="cart-layout">
       <div>
         <h1>Your Cart</h1>
-        {cart.length == 0 && <p>You have not added any product to your cart yet.</p>}
+        {cart.length === 0 && <p>You have not added any product to your cart yet.</p>}
       </div>
-      <table class="table table-cart">
-        <thead>
-          <tr>
-            <th width="25%" class="th-product">Product</th>
-            <th width="20%">Unit price</th>
-            <th width="10%">Quanity</th>
-            <th width="25%">Total</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cart.map(product=>{
-            console.log(cart)
-            return(
-              <tr>
-                <td>
-                  <img width="30" height="30" alt="" src={product.image}/>
-                  {product.name}
-                </td>
-                <td>${product.price}</td>
-                <td>{product.quantity}</td>
-                <td>
-                  <strong>${product.price*product.quantity}</strong>
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-        <tfoot>
-          <tr>
-            <th colSpan="2"></th>
-            <th class="cart-highlight">{totalQuantity}</th>
-            <th class="cart-highlight">${totalPrice}</th>
-          </tr>
-        </tfoot>
-      </table>
+      {cart.length > 0 && <>
+        <table class="table table-cart">
+          <thead>
+            <tr>
+              <th width="25%" class="th-product">Product</th>
+              <th width="20%">Unit price</th>
+              <th width="10%">Quanity</th>
+              <th width="25%">Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map(product=>{
+              console.log(cart)
+              return(
+                <tr>
+                  <td>
+                    <img width="30" height="30" alt="" src={product.image}/>
+                    {product.name}
+                  </td>
+                  <td>${product.price}</td>
+                  <td>{product.quantity}</td>
+                  <td>
+                    <strong>${product.price*product.quantity}</strong>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+          <tfoot>
+            <tr>
+              <th colSpan="2"></th>
+              <th class="cart-highlight">{totalQuantity}</th>
+              <th class="cart-highlight">${totalPrice}</th>
+            </tr>
+          </tfoot>
+        </table>
+        <form class="pay-form" onSubmit={handlePayClick}>
+              <p>
+                Enter your email and then click on pay and your products will be
+                delivered to you on the same day!
+              </p>
+              <Input autocomplete="email" placeholder="Email" type="email" required onChange={handleEmailChange} value={email}/>
+              <Button type="submit">Pay</Button>
+            </form>
+      </>}
     </div>
   );
 }
