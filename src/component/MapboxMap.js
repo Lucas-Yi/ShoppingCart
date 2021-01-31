da6ca066220425dba3f49aaef68952faf7c56bd0
Lucas_Yi <423188112@qq.com>
2021-01-31 16:30:38 +1100

    fix bug with mapboxgl in dev mode
    Uncaught ReferenceError: y is not defined

--- src/component/MapboxMap.js
+++ src/component/MapboxMap.js
@@ -3,6 +3,9 @@ import React, {useLayoutEffect} from "react"
 
 export default function MapboxMap(){
 
+    // @ts-ignore
+    // eslint-disable-next-line import/no-webpack-loader-syntax
+    mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
     mapboxgl.accessToken = "pk.eyJ1IjoibHVjYXN5aTE5OTIiLCJhIjoiY2trNmN4b3o4MDJwbzJ1cW80OGR3MGwzZiJ9.f3ApJAfmbuuOj4FdKs60nA"
 
     useLayoutEffect(() => {
